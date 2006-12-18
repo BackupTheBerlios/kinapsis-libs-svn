c0866c9c4f5e309d5c40362215c470fe015c9e50
luisc <luisc@ebefd6de-af0d-0410-b22e-fd1a0c93818a>
2006-12-18 16:26:25 +0000

    aham

---
+++ liboscar/trunk/src/oftparser.cpp
@@ -0,0 +1,49 @@
+/***************************************************************************
+ *   Copyright (C) 2006 by Luis Cidoncha                                   *
+ *   luis.cidoncha@gmail.com                                               *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+
+#include "oftparser.h"
+
+namespace liboscar {
+
+OFTParser::OFTParser(Client *c) : ParserBase(c) { }
+
+OFTParser::~OFTParser() { }
+
+void OFTParser::parse() {
+	// Parses an OFT command
+
+	DWord dw;
+	Word w, len;
+
+	m_buf >> dw;
+
+	if (dw != 0x4f465432) return; // this is a version we don't understand
+
+	m_buf >> len; // total len of data
+
+	len -= 6;
+
+
+}
+
+}
+
+#include "oftparser.moc"
