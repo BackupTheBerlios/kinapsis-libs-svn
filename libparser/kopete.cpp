a7e147625f4c5817a5393e34a92ef271d719a09a
narayan <narayan@ebefd6de-af0d-0410-b22e-fd1a0c93818a>
2008-04-08 15:25:47 +0000

    Metacontacts

--- libparser/kopete.cpp
+++ libparser/kopete.cpp
@@ -1,3 +1,23 @@
+/***************************************************************************
+ *   Copyright (C) 2008 by David Menéndez González                         *
+ *   narayan82@gmail.com                                                   *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
 #include <iostream>
 #include <QtDebug>
 #include <QFile>
@@ -125,11 +145,12 @@ using namespace std;
 
             void Kopete::processMetacontacts(QDomElement e){
                     QDomAttr a;
-//                    qDebug() << e.text() << endl; 
+                    QString tmp;
                     a = e.attributeNode("contactId");
-                    //FIXME
-                    if (a.specified()){
-                        qDebug() << a.value() << endl; 
+//                    if (a.specified()){ //This should work
+                    if (!a.value().isEmpty()){
+                        tmp.append(a.value());
+                        m_metacontacts << tmp;
                     }
 
             }
